64c4121f5202d7fe7208d5e0d0a3a4441c5547cb
inbum <sib0817@naver.com>
2023-02-12 01:43:17 +0900

    tutorial!

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+view = [300,200,100,500]
+st.write('# raw data')
+view
+st.write('# table')
+st.table(view)  
+
+st.write('# bar graph')
+st.bar_chart(view)
+
+show_raw=st.checkbox('show raw data')
+if show_raw==True:
+    st.write('# raw data')
+    view
